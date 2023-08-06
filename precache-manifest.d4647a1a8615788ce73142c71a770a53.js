9b6f81532a9adca607e3c0a8ba8c6f21be8cb8ab
unknown <nandanani1124@gmail.com>
2023-08-06 12:20:27 +0530

    Updates

---
+++ precache-manifest.d4647a1a8615788ce73142c71a770a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56f9f3dc9189b7e381d1",
+    "url": "/Task_Tracker/static/css/main.d72ce9f3.chunk.css"
+  },
+  {
+    "revision": "56f9f3dc9189b7e381d1",
+    "url": "/Task_Tracker/static/js/main.56f9f3dc.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/Task_Tracker/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "598c3cb8ab3494be0837",
+    "url": "/Task_Tracker/static/js/2.598c3cb8.chunk.js"
+  },
+  {
+    "revision": "1bb9a108a9c45006b513",
+    "url": "/Task_Tracker/static/js/runtime~main.1bb9a108.js"
+  },
+  {
+    "revision": "a789f05bbe8cdbe99dc5b0993a2c5a7c",
+    "url": "/Task_Tracker/index.html"
+  }
+];
